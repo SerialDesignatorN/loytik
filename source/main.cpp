92d4583db9f1327d0029ee78f84ab7958daa84e7
zeankun.dev <zeanfender11@gmail.com>
2022-06-14 07:20:04 +0700

    fix only exec message when executed with no args available

--- source/main.cpp
+++ source/main.cpp
@@ -3,26 +3,28 @@
 #include "standard_functions.hpp"
 
 int main(int c, char* argv[]) {
-	std::string version = "0.9.6";
-	// load the path from shell
-	// schema: ./loyitik /path/to/file.lytk
+	// if arg is blank display schema
+	if (c == 1) {
+		printf("Schema: loytik [OPTIONS] file\n");
+		printf("Type -h for possible options\n");
+		return 0;
+	}
+	std::string version = "0.9.8";
 	std::string path = argv[1];
 	path = path.substr(0);
-	// check if any flags are passed
-	// example: -v or --version
-	// schema: ./loyitik -v
 	if (c == 2) {
 		if (argv[1][0] == '-' && argv[1][1] == 'v') {
-			std::cout << "Loyitik Live Interpreter v" << version << "\nby zeankun.dev" << std::endl;
+			std::cout << "Loytik Live Interpreter v" << version << "\nby zeankun.dev" << std::endl;
 			return 0;
 		}
 		// -h
 		if (argv[1][0] == '-' && argv[1][1] == 'h') {
-			printf("Loyitik Live Interpreter v%s\nby zeankun.dev\n\n", version.c_str());
-			printf("Schema: ./loyitik [options] [file]\n\n");
+			printf("Loytik Live Interpreter v%s\nby zeankun.dev\n\n", version.c_str());
+			printf("Schema: loyitik [options] [file]\n\n");
 			printf("Options:\n");
 			printf("-h = Help \n");
 			printf("-v = Version\n");
+			return 0;
 		}
 	}
 	using namespace loytik;
